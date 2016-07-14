2e5f7dc196a8bd8881f9b84383b69d1416b027d3
Paul Thorpe <paulsthorpe@yahoo.com>
2016-07-14 12:10:16 -0400

    fresh themosis project

---
+++ .env.local.php
@@ -0,0 +1,15 @@
+<?php
+
+/*----------------------------------------------------*/
+// Local environment vars
+/*----------------------------------------------------*/
+return [
+
+    'DB_NAME'       => 'themosis',
+    'DB_USER'       => 'root',
+    'DB_PASSWORD'   => 'jas123',
+    'DB_HOST'       => 'localhost',
+    'WP_HOME'       => 'http://localhost/themosis/htdocs/',
+    'WP_SITEURL'    => 'http://localhost/themosis/htdocs/cms'
+
+];
---
+++ config/environment.php
@@ -0,0 +1,11 @@
+<?php
+
+/*----------------------------------------------------*/
+// Define your environments
+/*----------------------------------------------------*/
+return [
+
+    'local'         => 'paul-Q502LA',
+    'production'    => 'your-production-hostname'
+
+];
